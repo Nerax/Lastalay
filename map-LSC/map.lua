dc1dd9b8786086cb10208c85dd88610675283eba
Nerax <reshetkoz@mail.ru>
2018-10-21 01:55:59 +0300

    Add files via upload

---
+++ map-LSC/map.lua
@@ -0,0 +1,6 @@
+spawnpoint 'mp_m_freemode_01' { x = -387, y = -103, z = 58 }
+spawnpoint 'mp_f_freemode_01' { x = -383, y = -102, z = 58 }
+spawnpoint 'mp_m_freemode_01' { x = -380, y = -104, z = 58 }
+spawnpoint 'mp_f_freemode_01' { x = -390, y = -110, z = 58 }
+
+--
